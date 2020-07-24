28f6f1972ebedf3fff7edf2eab251fc9beaa19a9
Yeo Inbo <yeo.inbo@victech.io>
2020-07-24 17:51:49 +0900

    add router-link navigation

---
+++ tools/README.md
@@ -0,0 +1,11 @@
+# toosl
+
+## nodejs install
+```
+> curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+> sudo apt-get install -y nodejs
+> npm --version
+6.14.6
+> node --version
+v12.18.2
+```
--- tools/annofront/src/App.vue
+++ tools/annofront/src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <p>
+      <!-- 네비게이션을 위해 router-link 컴포넌트를 사용합니다. -->
+      <!-- 구체적인 속성은 `to` prop을 이용합니다. -->
+      <!-- 기본적으로 `<router-link>`는 `<a>` 태그로 렌더링됩니다.-->
+      <router-link to="/" tag="button">Editor</router-link>--
+      <router-link to="/help" tag="button">Help</router-link>
+    </p>
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <router-view></router-view>
@@ -8,12 +15,10 @@
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    //HelloWorld
   }
 }
 </script>
